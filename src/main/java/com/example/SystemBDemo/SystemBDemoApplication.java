84435508d4192fa8ab7684d5e5749a6fb635252e
Flavio Delhusa <flavio.delhusa@gmail.com>
2025-12-08 15:08:52 +0100

    Initial commit

---
+++ src/main/java/com/example/SystemBDemo/SystemBDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SystemBDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SystemBDemo/SystemBDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SystemBDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
